01597739c3c069076e051095241c034b8614f9fa
Camila Zelenovsky <camila_zele@hotmail.com>
2020-07-27 23:44:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe seu salário em R$:");
+            double payment = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe a porcentagem de reajuste:");
+            double readjustment = Convert.ToDouble(Console.ReadLine());
+            double finalreadjustment = readjustment / 100;
+
+            double finalpayment = payment + (payment * finalreadjustment);
+            finalpayment = Math.Round(finalpayment, 2);
+            Console.WriteLine($"O seu reajust é de {finalpayment}");
+
+        }
+    }
+}
